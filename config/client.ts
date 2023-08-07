0051534c229fe7800ce4f516d50e5471f23a7b5a
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2023-08-07 18:44:21 +0300

    add timeouts

--- config/client.ts
+++ config/client.ts
@@ -4,14 +4,20 @@ import { gnosis, mainnet, sepolia } from "viem/chains";
 export const publicClient = {
   [mainnet.id]: createPublicClient({
     chain: mainnet,
-    transport: http(process.env.PRIVATE_RPC_ENDPOINT_MAINNET),
+    transport: http(process.env.PRIVATE_RPC_ENDPOINT_MAINNET, {
+      timeout: 600_000,
+    }),
   }),
   [gnosis.id]: createPublicClient({
     chain: gnosis,
-    transport: http(process.env.PRIVATE_RPC_ENDPOINT_GNOSIS),
+    transport: http(process.env.PRIVATE_RPC_ENDPOINT_GNOSIS, {
+      timeout: 600_000,
+    }),
   }),
   [sepolia.id]: createPublicClient({
     chain: sepolia,
-    transport: http(process.env.PRIVATE_RPC_ENDPOINT_SEPOLIA),
+    transport: http(process.env.PRIVATE_RPC_ENDPOINT_SEPOLIA, {
+      timeout: 600_000,
+    }),
   }),
 };
