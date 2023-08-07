30657f8760c73d9f7f42d1702ec2c0dfe380854f
geaxed <98810017+geaxed@users.noreply.github.com>
2023-08-07 14:59:07 +0200

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NOTICE_METAEVIDENCE_URL: string;
       DATALAKE_URL: string;
       DATALAKE_KEY: string;
       LOGTAIL_SOURCE_TOKEN: string;
