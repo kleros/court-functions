2d4f5f2b0b94ad0605c6282901f36d2665080da7
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2023-08-14 17:17:41 +0300

    replace dynamodb for params check

--- config/dynamoDB.ts
+++
@@ -1,8 +0,0 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-
-export const dynamoDB = new DynamoDBClient({
-  credentials: {
-    accessKeyId: process.env.DDB_ACCESS_KEY,
-    secretAccessKey: process.env.DDB_SECRET_KEY,
-  },
-});
--- functions/put-justification.ts
+++ functions/put-justification.ts
@@ -12,13 +12,11 @@ import { gnosis, mainnet, sepolia } from "viem/chains";
 import { publicClient } from "../config/client";
 import { klerosAddress } from "../config/contracts";
 import { validateChainId } from "../utils/validate";
-import { dynamoDB } from "../config/dynamoDB";
-import { GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { datalake } from "../config/supabase";
 import logtail from "../config/logtail";
 
 const headers = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": process.env.COURT_INSTANCE_URL,
 };
 
 const chainIds = [mainnet.id, gnosis.id, sepolia.id];
@@ -30,16 +28,15 @@ const chainName = {
 };
 
 interface RequestBody {
-  payload: {
-    chainId: Supported<typeof chainIds>;
-    address: Address;
-    signature: Address;
-    justification: {
-      disputeID: number;
-      appeal: number;
-      voteIDs: number[];
-      justification: string;
-    };
+  chainId: Supported<typeof chainIds>;
+  account: Address;
+  derived?: Address;
+  signature: Address;
+  justification: {
+    disputeID: number;
+    appeal: number;
+    voteIDs: number[];
+    justification: string;
   };
 }
 
@@ -63,33 +60,24 @@ export const handler: Handler = async (ev) => {
       );
     if (!ev.body) throw new Error("No body provided");
 
-    const { payload } = JSON.parse(ev.body) as RequestBody;
-
     const {
       chainId: chainIdParam,
-      address,
+      account,
+      derived,
       signature,
       justification: { disputeID, appeal, voteIDs, justification },
-    } = payload;
+    } = JSON.parse(ev.body) as RequestBody;
 
     const chainId = validateChainId(String(chainIdParam), chainIds);
 
-    const sender = await recoverAddress({
+    const recovered = await recoverAddress({
       hash: keccak256(
         toHex(JSON.stringify({ disputeID, appeal, voteIDs, justification }))
       ),
       signature,
     });
-    const derived = (
-      await dynamoDB.send(
-        new GetItemCommand({
-          TableName: "user-settings",
-          Key: { address: { S: address } },
-          ProjectionExpression: "derivedAccountAddress",
-        })
-      )
-    ).Item?.derivedAccountAddress.S;
-    if (sender !== derived)
+
+    if (recovered !== account || recovered !== derived)
       throw new Error(
         "The sender address does not match the address in the payload"
       );
@@ -102,7 +90,7 @@ export const handler: Handler = async (ev) => {
         BigInt(voteId),
       ]);
 
-      if (voter !== address || voted)
+      if (voter !== account || voted)
         throw new Error(
           "Not all of the supplied vote IDs belong to the supplied address and are not cast."
         );
@@ -125,7 +113,7 @@ export const handler: Handler = async (ev) => {
     return {
       headers,
       statusCode: StatusCodes.OK,
-      body: JSON.stringify({ payload: payload.justification }),
+      body: JSON.stringify({ disputeID, appeal, voteIDs, justification }),
     };
   } catch (err: any) {
     logtail.error("Error occurred", { error: err.message });
